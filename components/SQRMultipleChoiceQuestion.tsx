b6578a11b9635293a66fd5db90c8c63931eca54b
Ian Kim <me@ian.kim>
2023-06-13 17:51:53 -0400

    Scaffold SQR-specific MC, QD components

---
+++ components/SQRMultipleChoiceQuestion.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const SQRMultipleChoiceQuestion: React.VFC<Props> = () => {
+	return null;
+};
+
+export default SQRMultipleChoiceQuestion;
---
+++ components/SQRQuestionDeck.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const SQRQuestionDeck: React.VFC<Props> = () => {
+	return null;
+};
+
+export default SQRQuestionDeck;

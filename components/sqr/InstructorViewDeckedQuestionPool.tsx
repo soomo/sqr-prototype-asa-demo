8568c240eb0197b33a71fa4bcb7045f7598d13d5
Ian Kim <me@ian.kim>
2023-07-18 09:40:03 -0400

    Port over styles for instructor view decked QP

--- components/sqr/InstructorViewDeckedQuestionPool.tsx
+++ components/sqr/InstructorViewDeckedQuestionPool.tsx
@@ -70,13 +70,19 @@ const InstructorViewDeckedQuestionPool: React.VFC<Props> = ({
 					onChangeSelectedChoice={() => {}}
 					questionFamilyId={activeQuestion.familyId}
 					selectedChoiceFamilyId={activeQuestionCorrectChoice.familyId}
+					css={choicesStyles}
 				/>
-				<Rejoinder correct={true} rejoinder={activeQuestionCorrectChoice.rejoinder} />
+				<Rejoinder
+					correct={true}
+					rejoinder={activeQuestionCorrectChoice.rejoinder}
+					css={rejoinderStyles}
+				/>
 				<InstructorViewPoolNavigation
 					onPrevious={handlePrevious}
 					onNext={handleNext}
 					activeIndex={activePoolIndex}
 					numQuestions={poolElement.questions.length}
+					css={instructorViewPoolNavigationStyles}
 				/>
 			</div>
 		</div>
@@ -85,4 +91,72 @@ const InstructorViewDeckedQuestionPool: React.VFC<Props> = ({
 
 export default InstructorViewDeckedQuestionPool;
 
-const styles = css``;
+const styles = css`
+	border: 1px solid #c9c9c9;
+	border-radius: 0.5rem;
+
+	.prompt-and-pivotar {
+		position: relative;
+		display: grid;
+		padding: 1rem 1.5rem;
+		grid-template-columns: 1fr auto;
+		align-items: flex-start;
+		column-gap: 1.5rem;
+		font: inherit;
+		border: none;
+		background: none;
+		cursor: pointer;
+		text-align: initial;
+
+		&[aria-expanded='false'][data-answered='true'] .correctness-and-prompt .question-body {
+			display: -webkit-box;
+			overflow: hidden;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 1;
+			color: rgba(0, 0, 0, 0.5);
+		}
+
+		.correctness {
+			position: absolute;
+			left: -13px;
+			display: inline-flex;
+			margin-right: 0.75rem;
+			align-items: baseline;
+			font-size: 18px;
+			line-height: 30px;
+			font-weight: 500;
+			font-style: italic;
+			column-gap: 0.5rem;
+
+			svg {
+				width: 27px;
+				height: 27px;
+			}
+		}
+
+		// QuestionPrompt outer div
+		div:last-of-type {
+			display: inline;
+		}
+
+		// QuestionPrompt inner div
+		.question-body {
+			margin: 0;
+		}
+	}
+`;
+
+const choicesStyles = css`
+	padding-left: 1.5rem;
+	padding-right: 2rem;
+`;
+
+const rejoinderStyles = css`
+	margin-right: 2rem;
+	padding-left: 1.5rem;
+`;
+
+const instructorViewPoolNavigationStyles = css`
+	margin-top: 1rem;
+	padding: 0 2rem 1.5rem 0;
+`;
